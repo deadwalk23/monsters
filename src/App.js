311dd235d05f2114ceb0d65e13b2787f61807be3
deadwalk23 <johnson12kalvin@gmail.com>
2021-06-01 19:53:10 +0530

    Adding Files

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ class App extends Component {
     return(
     
           <div className="App">
-            <h1>Monsters-Rolo</h1>
+            <h1>Monsters</h1>
             
             <SearchBox 
               placeholder='Search Monsters'
